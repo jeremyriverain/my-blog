a0c4e7fdad73ab4a4d25a8402e885203f4a6703f
Jérémy Riverain <jeremy@geekco.fr>
2021-07-11 21:02:24 +0200

    fix: percy

--- percyrc.js
+++ percyrc.js
@@ -13,15 +13,6 @@ module.exports = [
       'http://localhost:3000/posts/en/how-to-use-gitlab-ci-and-php-stan-to-improve-code-quality-in-your-symfony-project'
   },
   {
-    name: 'project list',
-    url: 'http://localhost:3000/projects'
-  },
-  {
-    name: 'project example',
-    waitForSelector: 'img[lazy=loaded]',
-    url: 'http://localhost:3000/projects/klubin'
-  },
-  {
     name: 'contact',
     url: 'http://localhost:3000/contact'
   }
