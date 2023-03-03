cb586776f2f5b773176615e9b2239cd9e3d853c8
Jérémy Riverain <jeremy@geekco.fr>
2023-03-03 20:55:31 +0100

    🐛 fix tests

--- playwright/navigation.spec.ts
+++ playwright/navigation.spec.ts
@@ -1,4 +1,4 @@
-import { test } from '@playwright/test';
+import { expect, test } from '@playwright/test';
 
 test('navigation', async ({ page }) => {
   await page.goto('/');
@@ -6,6 +6,10 @@ test('navigation', async ({ page }) => {
   await page.getByRole('link', { name: 'Contact' }).click();
   await page.getByRole('link', { name: 'Get in touch' }).click();
   await page.getByRole('link', { name: 'Articles' }).click();
-  await page.getByRole('link', { name: 'Create forms faster with Formkit' }).nth(1).click();
-  await page.getByRole('heading', { name: 'Create forms faster with Formkit' }).click();
+  const link = await page.locator('.articles-list .featured h1')
+  const postTitle = await link.innerText()
+  const url = await page.url()
+  await link.click()
+  await page.getByRole('heading', { name: postTitle}).click();
+  await expect(page.url()).not.toBe(url)
 });
